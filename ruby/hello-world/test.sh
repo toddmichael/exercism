d2680799a144ebc151a168da89adb3ff560a0260
Todd Michael Bushnell <toddmichael@gmail.com>
2016-07-03 08:10:25 -0700

    replace per-dir test.sh scripts w/ single script + symlinks
    DRY this out a bit

--- ruby/hello-world/test.sh
+++ ruby/hello-world/test.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-set -ex
-
-my_file=$(find . -type f -name '*.rb' ! -name '*_test.rb')
-my_test_file=$(find . -type f -name '*_test.rb')
-
-rubocop -Da $my_file
-ruby -rminitest/pride $my_test_file
+../bin/test.sh
