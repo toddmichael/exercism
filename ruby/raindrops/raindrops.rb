bff430aa789914c6dd29d7d8a007de5279ae5244
Todd Michael Bushnell <toddmichael@gmail.com>
2016-07-04 14:29:13 -0700

    ruby raindrops (#5) solution

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,25 @@
+# exercism #5
+
+# version tracker
+module BookKeeping
+  VERSION = 2
+end
+
+# something
+class Raindrops
+  # factor-to-sound lookup table
+  @table = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
+
+  # convert number to its corresponding sound/string
+  def self.convert(number)
+    response = ''
+    @table.each do |factor, sound|
+      response.concat(sound) if number.modulo(factor) == 0
+    end
+    response.empty? ? number.to_s : response
+  end
+end
